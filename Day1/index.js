a8187afd9860288f3218a4c735ac228ecb50b3fa
Andreas <andreas.casenarg@gmail.com>
2019-12-19 13:11:41 +0100

    working on day 1, exercise 2. Function calcFuelForFuel is doing something wrong

--- Day1/index.js
+++ Day1/index.js
@@ -2,15 +2,27 @@ const { day1Inputs } = require('./data')
 
 /**
  * @function calculateFuel
+ * @description - Calculates the amout of fuel needed for a specific mass
  * @param { Number } mass
  */
 const calculateFuel = (mass) => Math.floor(mass / 3) - 2
 
+/**
+ * @function calcFuelForModules
+ * @description - Calculates the total fuel needed for an array of modules with different masses
+ * @param { Number[] } modules 
+ */
 const calcFuelForModules = modules => modules.reduce((totalFuelNeeded, singleModule) => {
 	totalFuelNeeded += calculateFuel(singleModule)
 	return totalFuelNeeded
 }, 0)
 
+/**
+ * @function calcFuelForModules
+ * @description - Calculates the extra fuel needed once the mass of the initial fuel is taken into account
+ * @param { Number } totalFuel
+ * @param { Number } lastFuelAdded
+ */
 const calcFuelForFuel = (totalFuel, lastFuelAdded) => {
 	let extraFuelNeeded = calculateFuel(lastFuelAdded)
 	if (extraFuelNeeded <= 0) {
@@ -27,6 +39,12 @@ const calcFuelForFuel = (totalFuel, lastFuelAdded) => {
 	return calcFuelForFuel(newTotalFuel, extraFuelNeeded)
 }
 
+/**
+ * @function calculateTotalFuel
+ * @description - Calculates the toal fuel needed to carry the total mass of an array of modules
+ * plus the mass of the fuel needed to carry them.
+ * @param { Number[] } modules
+ */
 const calculateTotalFuel = (modules) => {
 	const initialFuelNeeded = calcFuelForModules(modules)
 	const fuel4Fuel = calcFuelForFuel(initialFuelNeeded, initialFuelNeeded)
