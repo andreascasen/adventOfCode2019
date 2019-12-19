a70ebb65693429fff826fa7865833fe922f0511a
Andreas <andreas.casenarg@gmail.com>
2019-12-19 13:08:50 +0100

    working on day 1, exercise 2. Function calcFuelForFuel is doing something wrong

--- Day1/index.js
+++ Day1/index.js
@@ -6,9 +6,37 @@ const { day1Inputs } = require('./data')
  */
 const calculateFuel = (mass) => Math.floor(mass / 3) - 2
 
-const calculateTotalFuel = modules => modules.reduce((totalFuelNeeded, singleModule) => {
+const calcFuelForModules = modules => modules.reduce((totalFuelNeeded, singleModule) => {
 	totalFuelNeeded += calculateFuel(singleModule)
 	return totalFuelNeeded
 }, 0)
+
+const calcFuelForFuel = (totalFuel, lastFuelAdded) => {
+	let extraFuelNeeded = calculateFuel(lastFuelAdded)
+	if (extraFuelNeeded <= 0) {
+		/** No fuel needs to be added */
+		return totalFuel
+	}
+	console.log('info => ', {
+		totalFuel,
+		lastFuelAdded,
+		extraFuelNeeded
+	})
+	
+	const newTotalFuel = totalFuel + extraFuelNeeded
+	return calcFuelForFuel(newTotalFuel, extraFuelNeeded)
+}
+
+const calculateTotalFuel = (modules) => {
+	const initialFuelNeeded = calcFuelForModules(modules)
+	const fuel4Fuel = calcFuelForFuel(initialFuelNeeded, initialFuelNeeded)
+	console.log('Data => ', {
+		initialFuelNeeded,
+		fuel4Fuel
+	})
+	const totalFuel = initialFuelNeeded + fuel4Fuel
+	console.log('Total Fuel => ', totalFuel)
+	return totalFuel
+}
 
 calculateTotalFuel(day1Inputs) // 3393938
