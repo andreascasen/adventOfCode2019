7d8aa0b4ccf2c6b40973e3d4f8f848e21a36d3b0
Andreas <andreas.casenarg@gmail.com>
2019-12-19 21:26:48 +0100

    day 2 solved

--- Day1/index.js
+++ Day1/index.js
@@ -1,60 +1,18 @@
 const { day1Inputs } = require('./data')
 
-/**
- * @function calculateFuel
- * @description - Calculates the amout of fuel needed for a specific mass
- * @param { Number } mass
- */
-const calculateFuel = (mass) => Math.floor(mass / 3) - 2
-
-/**
- * @function calcFuelForModules
- * @description - Calculates the total fuel needed for an array of modules with different masses
- * @param { Number[] } modules 
- */
-const calcFuelForModules = modules => modules.reduce((totalFuelNeeded, singleModule) => {
-	totalFuelNeeded += calculateFuel(singleModule)
-	return totalFuelNeeded
-}, 0)
-
-/**
- * @function calcFuelForModules
- * @description - Calculates the extra fuel needed once the mass of the initial fuel is taken into account
- * @param { Number } totalFuel
- * @param { Number } lastFuelAdded
- */
-const calcFuelForFuel = (totalFuel, lastFuelAdded) => {
-	let extraFuelNeeded = calculateFuel(lastFuelAdded)
-	if (extraFuelNeeded <= 0) {
-		/** No fuel needs to be added */
-		return totalFuel
+const calculateTotalFuel = function(mass) {
+	const fuelNeeded = Math.floor(mass / 3) - 2
+	let totalFuel = 0
+  
+	if (fuelNeeded > 0) {
+		totalFuel += fuelNeeded + calculateTotalFuel(fuelNeeded)
 	}
-	console.log('info => ', {
-		totalFuel,
-		lastFuelAdded,
-		extraFuelNeeded
-	})
-	
-	const newTotalFuel = totalFuel + extraFuelNeeded
-	return calcFuelForFuel(newTotalFuel, extraFuelNeeded)
-}
-
-/**
- * @function calculateTotalFuel
- * @description - Calculates the toal fuel needed to carry the total mass of an array of modules
- * plus the mass of the fuel needed to carry them.
- * @param { Number[] } modules
- */
-const calculateTotalFuel = (modules) => {
-	const initialFuelNeeded = calcFuelForModules(modules)
-	const fuel4Fuel = calcFuelForFuel(initialFuelNeeded, initialFuelNeeded)
-	console.log('Data => ', {
-		initialFuelNeeded,
-		fuel4Fuel
-	})
-	const totalFuel = initialFuelNeeded + fuel4Fuel
-	console.log('Total Fuel => ', totalFuel)
+  
 	return totalFuel
 }
+  
+fuelRequirements = function(modules) {
+	return modules.reduce((totalFuel, singleModule) => totalFuel + calculateTotalFuel(singleModule), 0)
+}
 
-calculateTotalFuel(day1Inputs) // 3393938
+console.log(fuelRequirements(day1Inputs)) // 3393938
---
+++ Day2/day2.js
@@ -0,0 +1,68 @@
+const { data, test1, test2, test3 } = require('./data')
+
+data[1] = 12
+data[2] = 2
+
+const intCodeComputer = (intCode) => {
+	let currentPosition = 0
+	let firstValue
+	let secondValue
+	let resultPosition
+	
+	const parseIntCode = () => {
+		switch (intCode[currentPosition]) {
+			case 1:
+				firstValue = intCode[intCode[currentPosition + 1]]
+				secondValue = intCode[intCode[currentPosition + 2]]
+				resultPosition = intCode[currentPosition + 3]
+				intCode[resultPosition] = firstValue + secondValue
+				currentPosition += 4
+				parseIntCode()
+				break
+			case 2:
+				firstValue = intCode[intCode[currentPosition + 1]]
+				secondValue = intCode[intCode[currentPosition + 2]]
+				resultPosition = intCode[currentPosition + 3]
+				intCode[resultPosition] = firstValue * secondValue
+				currentPosition += 4
+				parseIntCode()
+				break
+			default:
+				break
+		}		
+	}
+
+	parseIntCode()
+
+	return intCode
+}
+
+const findDesiredInputs = () => {
+	const desiredResult = 19690720
+	
+	let correctNoun = 0
+	let correctVerb = 0
+	let intCode = [...data]
+
+	loop1: for (let noun = 0; noun < 100; noun++) {
+		for (let verb = 0; verb < 100; verb++) {
+			intCode = [...data]
+
+			intCode[1] = noun
+			intCode[2] = verb
+
+			const intCodeZero = intCodeComputer(intCode)[0]
+
+			if (intCodeZero === desiredResult) {
+				correctNoun = noun
+				correctVerb = verb
+				break loop1
+			}
+		}
+
+	}
+
+	return 100 * correctNoun + correctVerb
+}
+
+console.log('Result => ', findDesiredInputs())
